d761bcfa6e825e03c3f92305ca3dd01c4258b333
Matt Hooper <matt@mhooper72.com>
2020-06-24 15:14:38 -0600

    chore(add dataloader)

--- server/dataLoader.js
+++ server/dataLoader.js
@@ -1,6 +1,7 @@
 import { ApolloServer, gql } from 'apollo-server'
 import models from 'db/models'
 import DataLoader from 'dataloader'
+import _ from 'lodash'
 
 const typeDefs = gql`
 	type Planet {
@@ -16,12 +17,16 @@ const typeDefs = gql`
 
 	type Query {
 		getAllSolarSystems: [SolarSystem]
+		getAllPlanetsByIds: [Planet]
 	}
 `
 
 const resolvers = {
 	Query: {
 		getAllSolarSystems: async (parent, args, { models }) => await models.SolarSystems.findAll(),
+		getAllPlanetsByIds: async (parent, args, { models }) => {
+			return await models.Planets.findAll({ where: { solarSystemId: [1, 2] } })
+		},
 	},
 	SolarSystem: {
 		planets: async (parent, args, context) => {
@@ -33,15 +38,36 @@ const resolvers = {
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
-	context: () => ({
-		planetsLoader: new DataLoader(async (keys) => {
-			console.log(`keys`, keys)
-			const planets = await models.Planets.findAll({ where: { solarSystemId: [keys] } })
-			console.log(planets)
-			return planets
-		}),
-		models,
-	}),
+	context: () => {
+		return {
+			planetsLoader: new DataLoader(async (keys) => {
+				const planets = await models.Planets.findAll({ where: { solarSystemId: keys } })
+
+				const planetMap = {}
+				planets.forEach(({ dataValues: planetData }) => {
+					if (planetMap[planetData.solarSystemId]) {
+						planetMap[planetData.solarSystemId] = planetMap[planetData.solarSystemId].concat(planetData)
+					} else {
+						planetMap[planetData.solarSystemId] = [planetData]
+					}
+				})
+				return keys.map((key) => planetMap[key])
+
+				// Alternate method with reduce, only one loop over the data but harder to quickly reason about
+				// return _.compact(
+				// 	planets.reduce((acc, { dataValues: planetData }) => {
+				// 		if (acc[planetData.solarSystemId]) {
+				// 			acc[planetData.solarSystemId] = acc[planetData.solarSystemId].concat(planetData)
+				// 		} else {
+				// 			acc[planetData.solarSystemId] = [planetData]
+				// 		}
+				// 		return acc
+				// 	}, [])
+				// )
+			}),
+			models,
+		}
+	},
 })
 
 models.sequelize.sync()
