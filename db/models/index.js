942c5dfefcc4ee88eb837c7fa86df68307b415ab
Matt Hooper <matt@mhooper72.com>
2020-06-24 15:40:35 -0600

    chore(add benchmarking)

--- db/models/index.js
+++ db/models/index.js
@@ -1,10 +1,15 @@
 import Sequelize from 'sequelize'
+import chalk from 'chalk'
 
 const db_name = 'restless'
 const sequelize = new Sequelize(db_name, null, null, {
 	storage: 'db/restless.sqlite3',
 	dialect: 'sqlite',
 	typeValidation: true,
+	logging: (log, benchmark) => {
+		console.table(`${chalk.blue(log)} in ${chalk.bold.magenta(`${benchmark}ms`)}`)
+	},
+	benchmark: true,
 })
 
 const db = {
